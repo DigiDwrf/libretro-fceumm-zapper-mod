0a47d7d3fc17797e0217b13016e33e12cd3975df
Carlos O'Connor <carlos.oconnor88@gmail.com>
2020-11-23 03:08:50 -0500

    lcd

--- src/drivers/libretro/libretro.c
+++ src/drivers/libretro/libretro.c
@@ -1622,9 +1622,8 @@ void get_mouse_input(unsigned port, uint32_t *zapdata)
 
 void get_lightgun_input(unsigned port, uint32_t* zapdata)
 {
-    for (int i = 0; i < 2; i++) {
-        zapdata[i] = input_cb(port, RETRO_DEVICE_JOYPAD, 0, lightgunmap[i].retro) ? bindmap[i].nes : 0;
-    }
+        zapdata = input_cb(port, RETRO_DEVICE_JOYPAD, 0, lightgunmap[0].retro) ? bindmap[0].nes : 0;
+        zapdata = input_cb(port, RETRO_DEVICE_JOYPAD, 0, lightgunmap[1].retro) ? bindmap[1].nes : 0 << 1;
 }
 
 static void FCEUD_UpdateInput(void)
